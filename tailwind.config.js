fd299262c778b99884d5f0f0c57e71a68ccb4795
MagnusThiemer <thiemer.jensen@gmail.com>
2022-02-01 11:06:40 +0100

    add fonts and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#094DA1',
+        'highlight': '#F3D654',
+        'text-dark': '#0C3B7B',
+        'setting-red': '#FF9B9B',
+        'setting-green': '#93EB9E',
+        'setting-blue': '#94CAEA',
+        'setting-purple': '#A594EA',
+        'setting-pink': '#DE95EB',
+        'setting-orange': '#EBD095'
+      }
+    },
   },
   plugins: [],
 }
