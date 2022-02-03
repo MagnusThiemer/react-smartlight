a23488c5f930bf66273daba95b0b5d348aa939bf
MagnusThiemer <thiemer.jensen@gmail.com>
2022-02-03 13:10:16 +0100

    add color icons to room cards

--- src/components/RoomIcon.js
+++ src/components/RoomIcon.js
@@ -1,18 +1,19 @@
-import { IoBedOutline } from 'react-icons/io5'
-import { FaCouch } from 'react-icons/fa'
-import { MdOutlineKitchen, MdOutlineBalcony } from 'react-icons/md'
-import { BiBath } from 'react-icons/bi'
-import { GiHouse } from 'react-icons/gi'
+import bed from '../assets/bed.svg'
+import house from '../assets/house.svg'
+import bathtub from '../assets/bathtube.svg'
+import kitchen from '../assets/kitchen.svg'
+import balcony from '../assets/balcony.svg'
+import room from '../assets/room.svg'
 
 const RoomIcon = (props) => {
     return ( 
         <>
-        {props.name === 'Bed-room' && <IoBedOutline className={props.className}/>}
-        {props.name === 'Living-room' && <FaCouch className={props.className}/>}
-        {props.name === 'Kitchen' && <MdOutlineKitchen className={props.className}/>}
-        {props.name === 'Bathroom' && <BiBath className={props.className}/>}
-        {props.name === 'Outdoor' && <GiHouse className={props.className}/>}
-        {props.name === 'Balcony' && <MdOutlineBalcony className={props.className}/>}
+        {props.name === 'Bed-room' && <img src={bed} alt='bed room'/>}
+        {props.name === 'Living-room' && <img src={room} alt='living room'/>}
+        {props.name === 'Kitchen' && <img src={kitchen} alt='kitchen'/>}
+        {props.name === 'Bathroom' && <img src={bathtub} alt='bathroom'/>}
+        {props.name === 'Outdoor' && <img src={house} alt='outdoor'/>}
+        {props.name === 'Balcony' && <img src={balcony} alt='balcony'/>}
         
         </>
      );
--- src/templates/RoomCard.js
+++ src/templates/RoomCard.js
@@ -8,13 +8,15 @@ const RoomCard = (props) => {
     localStorage.setItem(props.data.name, JSON.stringify(props.data))
 
     return ( 
-        <Link key={props.key} to={`/settings/${props.data.name}`} className="bg-white rounded-3xl h-36 w-36 p-4 shadow-lg shadow-slate-300">
+        <Link key={props.key} to={`/settings/${props.data.name}`} className="bg-white rounded-3xl h-36 w-36 p-4 shadow-lg shadow-slate-300 flex flex-col justify-between items-start">
             <RoomIcon name={props.data.name} className='text-5xl mb-3 text-text-dark'/>
-            <H3 input={props.data.name.replace('-',' ')} className='font-bold text-text-dark'/>
-            <P 
-                input={props.data.lights.length > 1 ? `${props.data.lights.length} lights` : `${props.data.lights.length} light`}  
-                className='text-highlight font-bold'
-            />
+            <div>
+                <H3 input={props.data.name.replace('-',' ')} className='font-bold text-text-dark'/>
+                <P 
+                    input={props.data.lights.length > 1 ? `${props.data.lights.length} lights` : `${props.data.lights.length} light`}  
+                    className='text-highlight font-bold'
+                />
+            </div>
         </Link>
      );
 }
