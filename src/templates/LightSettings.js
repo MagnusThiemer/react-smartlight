fb960cd225e186ae203b918d25a99956bc64d181
MagnusThiemer <thiemer.jensen@gmail.com>
2022-02-04 09:51:35 +0100

    Add LightSlider component

---
+++ src/templates/LightSettings.js
@@ -0,0 +1,15 @@
+
+import { useState } from 'react';
+
+import LightSlider from '../components/LightSlider';
+
+const LightSettings = () => {
+
+    return ( 
+        <div className="bg-background-offwhite h-28 w-full rounded-t-3xl">
+            <LightSlider />
+        </div>
+     );
+}
+ 
+export default LightSettings;
